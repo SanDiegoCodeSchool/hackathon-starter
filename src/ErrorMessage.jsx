bdda46b91b6a217adebefaded728a1190295c5a5
Diego <dflores7237@gmail.com>
2020-01-07 15:14:00 -0800

    made style changes and cleaned up code. I also added a few comments to the code.

--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function ErrorMessage(props){
     return (
         <div className='errorMessage'>
-            <p>OOOOh No, we cant find an breweries near the location you searched!!! Please click the button below to start a new search.</p>
-            <button type='button' name="returnToBreweryFinder" onClick={props.clickHandler}>Take me back!</button>
+            <p>Oh no, we cant find any breweries in our database near the location you searched!!! Please click the button below to start a new search.</p>
+            <button type='button' className='returnToBreweryFinderButton' name='returnToBreweryFinder' onClick={props.clickHandler}>Take me back!</button>
         </div>
-    )
-}
+    );
+};
--- src/app.jsx
+++ src/app.jsx
@@ -17,7 +17,6 @@ class App extends Component {
             currentLocation : [],
             displayView : 1,
          }
-        //  this.clickHandler=this.clickHandler.bind(this);
     };
 
     componentDidMount(){
@@ -26,7 +25,12 @@ class App extends Component {
             .then(response => response.data)
             .then(currentLocation => this.setState({ currentLocation }));
           }
-
+// The clickHandler function will look at the name of the button that was pressed using event.target.name. 
+//from there a code block will run depending on the button that was pushed. For example if the 'breweryQuickFindButton'
+// on the main page is pressed, then a code will execute a get request on our brewery finding API and update the 'localBrewery'
+//state with a list of the breweries supplied by the API. Then the code will update current view by updating the 'displayView' state
+// to view number two (which renders a list of the local breweries). The other buttons actions that are exectured here include the
+// 'returnToBreweryFinder' and 'breweryFindByUserInputButton' buttons.
     clickHandler(event){
         if (event.target.name == 'breweryQuickFindButton'){
             Axios
@@ -41,15 +45,22 @@ class App extends Component {
             Axios
             .get(`https://api.openbrewerydb.org/breweries?by_postal=${userEnteredZipcode}`)
             .then(response => response.data)
-            .then(localBrewery => this.setState({ localBrewery }));
-            if (this.state.localBrewery.length == 0) {
+            .then(localBrewery => this.setState({ localBrewery })); 
+            if (userEnteredZipcode.length !== 5){
+                alert('Please enter a 5 digit zipcode!')
+            } else if (this.state.localBrewery.length == 0){
                 this.setState({displayView : 3});
             } else {
-                this.setState({ displayView : 2});
-            };
+                this.setState({ displayView : 2 });
+            };    
         };
     };
     render(){
+        // the view that is displayed is rendered based on the state value for 'displayView'. If displayView
+        // is equal to 1, then the <BreweryFinder> component will render. This componenet is used to search 
+        //for a brewery. If displayView is equal to 2, then <BreweryLocations> will render which shows all the 
+        //local breweries we found. If displayView is equal to 3, then <ErrorMessage> will render which shows a
+        // message that no breweries were found in the area searched.
         let currentView;
         switch(this.state.displayView){
             case 1:
@@ -57,39 +68,39 @@ class App extends Component {
                 break;
             case 2:
                 currentView = <div>
-                                <div className='breweryLocationTable'>
-                                <div className='tableHeaders tableName'>Brewery Name</div>
-                                <div className='tableHeaders tableWebsite'>Brewery Website</div>
-                                <div className='tableHeaders tableAddress'>Brewery Address</div>
-                                <div className='tableHeaders tableMap'>Link to map</div>
-                              </div>
-                                {
-                                    this.state.localBrewery.map(breweryInfo => (
-                                        <BreweryLocations 
-                                        key={breweryInfo['id']}
-                                        name={breweryInfo['name']}
-                                        website={breweryInfo['website_url']}
-                                        street={breweryInfo['street']}
-                                        city={breweryInfo['city']}
-                                        state={breweryInfo['state']}
-                                        zip={breweryInfo['postal_code']}
-                                        longitude={breweryInfo['longitude']}
-                                        latitude={breweryInfo['latitude']}
-                                        />
-                                    ))
-                                }
-                                <button type='button' name="returnToBreweryFinder" onClick={this.clickHandler.bind(this)}>lets start a new search</button>
+                                    <div className='breweryLocationTable'>
+                                        <div className='tableHeaders tableName'>Brewery Name</div>
+                                        <div className='tableHeaders tableWebsite'>Brewery Website</div>
+                                        <div className='tableHeaders tableAddress'>Brewery Address</div>
+                                        <div className='tableHeaders tableMap'>Link to map</div>
+                                    </div>
+                                    {
+                                        this.state.localBrewery.map(breweryInfo => (
+                                            <BreweryLocations 
+                                            key={breweryInfo['id']}
+                                            name={breweryInfo['name']}
+                                            website={breweryInfo['website_url']}
+                                            street={breweryInfo['street']}
+                                            city={breweryInfo['city']}
+                                            state={breweryInfo['state']}
+                                            zip={breweryInfo['postal_code']}
+                                            longitude={breweryInfo['longitude']}
+                                            latitude={breweryInfo['latitude']}
+                                            />
+                                        ))
+                                    }
+                                    <button type='button' className='returnToBreweryFinderButton' name='returnToBreweryFinder' onClick={this.clickHandler.bind(this)}>Start a new brewery search!</button>
                               </div>
                 break;
             case 3:
                 currentView = <ErrorMessage clickHandler={this.clickHandler.bind(this)}/> 
                 break;
-        }
+        };
         return(
             <div className='container'>
                 <header>
-                    <h1>Welcome to breweryFinder</h1>
-                    <h3>The fastest way to find a brewery near you.</h3>
+                    <p className='headerTitle'>Welcome to breweryFinder</p>
+                    <p className='headerSubTitle'>The fastest way to find a brewery near you.</p>
                 </header>
                 <main>
                     {currentView}
@@ -97,7 +108,5 @@ class App extends Component {
             </div>
         );
     };
-
-
-}
+};
 export default App;
